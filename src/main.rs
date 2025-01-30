3a7e57ab101f45f492969631bc15b7781dd90eb3
intbyte-100 <66622928+intbyte-100@users.noreply.github.com>
2025-01-30 20:20:18 +0300

    base template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{form::Form, launch, routes};
+
+#[macro_use]
+extern crate rocket;
+
+#[derive(FromForm)]
+struct HelloWorldForm {
+    token: String
+}
+
+#[post("/hello_world", data = "<input>")]
+fn hello_world(input: Form<HelloWorldForm>) -> String {
+    input.token.clone()
+}
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![]).configure(rocket::Config {
+        address: "0.0.0.0".parse().unwrap(),
+        port: 8814,
+        ..Default::default()
+    })
 }
