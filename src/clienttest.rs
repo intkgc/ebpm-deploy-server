b6b2f2dcaedd98ebacb175f72dace04391e9054b
Kirill <kirbo06@mail.ru>
2025-02-07 19:30:59 +0100

    implement not basic file receiving and sending

--- src/clienttest.rs
+++ src/clienttest.rs
@@ -1,27 +1,36 @@
-use futures::stream::TryStreamExt;
-
-use reqwest::{Body, Client};
-
+use reqwest::{multipart, Client};
 use tokio::fs::File;
-
 use tokio_util::codec::{BytesCodec, FramedRead};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Открываем файл
     let file = File::open("example.txt").await?;
 
-    let client = reqwest::Client::new();
+    // Создаем поток данных из файла
+    let stream = FramedRead::new(file, BytesCodec::new());
+    let file_body = reqwest::Body::wrap_stream(stream);
+
+    // Создаем multipart форму
+    let form = multipart::Form::new()
+        .part(
+            "file",
+            multipart::Part::stream(file_body)
+                .file_name("example.txt")
+                .mime_str("text/plain")?,
+        )
+        .text("description", "Описание файла")
+        .text("user", "username");
+
+    // Отправляем запрос
+    let client = Client::new();
     let res = client
         .post("http://localhost:8814/upload")
-        .body(file_to_body(file))
+        .multipart(form)
         .send()
         .await?;
 
-    Ok(())
-}
+    println!("Ответ: {}", res.status());
 
-fn file_to_body(file: File) -> Body {
-    let stream = FramedRead::new(file, BytesCodec::new());
-    let body = Body::wrap_stream(stream);
-    body
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
-use rocket::fs::TempFile;
-use rocket::launch;
-use rocket::{post, routes};
+#[macro_use]
+extern crate rocket;
 
-#[post("/upload", data = "<file>")]
-async fn upload(mut file: TempFile<'_>) -> std::io::Result<()> {
-    file.persist_to("uploads/file.txt").await?;
-    Ok(())
+use rocket::http::ContentType;
+use rocket::Data;
+use rocket_multipart_form_data::{
+    MultipartFormData, MultipartFormDataField, MultipartFormDataOptions,
+};
+
+#[post("/upload", data = "<data>")]
+async fn upload(content_type: &ContentType, data: Data<'_>) -> std::io::Result<String> {
+    let options = MultipartFormDataOptions::with_multipart_form_data_fields(vec![
+        MultipartFormDataField::file("file"),
+        MultipartFormDataField::text("token"),
+    ]);
+
+    let mut form = MultipartFormData::parse(content_type, data, options)
+        .await
+        .unwrap();
+
+    if let Some(file_fields) = form.files.get("file") {
+        let file_field = &file_fields[0];
+        let file_path = format!(
+            "uploads/{}",
+            file_field
+                .file_name
+                .as_ref()
+                .unwrap_or(&"uploaded_file".to_string())
+        );
+        std::fs::rename(&file_field.path, &file_path)?;
+    }
+
+    let token = form
+        .texts
+        .remove("token")
+        .map(|v| v[0].text.clone())
+        .unwrap_or_default();
+
+    Ok(format!("Файл загружен пользователем {}", token))
 }
 
 #[launch]
